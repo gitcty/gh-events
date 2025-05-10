34e641194e59557d0b2ee0df108c0bc3d2e8d86e
gitcty <gitcty@gmail.com>
2025-05-10 12:47:45 -0400

    practice makes it perfect

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
           <img src={logo} />
         </div>
         <h1>Learn & Master GitHub Actions</h1>
+        <h3>Practice makes it perfect</h3>
       </header>
       <MainContent />
     </>
